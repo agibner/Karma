acfb35354b949d1d5cdb11b767a8b56cc23a5166
HoustonPutman <houstonputman@gmail.com>
2014-11-30 22:04:01 -0600

    Adding Hello World.

--- src/GroceryDemo.scala
+++ src/GroceryDemo.scala
@@ -3,9 +3,29 @@ package groceryinternal
 object GroceryDemo extends GroceryListInternal{
   def main(args: Array[String]){
     GROCERY_LIST
-    Watermelon
-    Noodles - 12
-    Apples 
+    Noodles - 72
+    Pasta
+    Noodles - 101
+    Pasta
+    Noodles - 108
+    Pasta
+    Noodles - 108
+    Pasta
+    Noodles - 111
+    Pasta
+    Noodles - 32
+    Pasta
+    Noodles - 87
+    Pasta
+    Noodles - 111
+    Pasta
+    Noodles - 114
+    Pasta
+    Noodles - 108
+    Pasta
+    Noodles - 100
+    Pasta
+    Noodles - 46
     Pasta
     Turkey
     AND_PROBABLY_OTHER_STUFF
