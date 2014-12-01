84915b3fd196c45f57b641d3cf0aa2fa9bc3afe9
Houston Putman <houstonputman@gmail.com>
2014-12-01 11:09:00 -0600

    Added a loop test

---
+++ src/LoopDemo.scala
@@ -0,0 +1,19 @@
+package groceryinternal
+
+object LoopDemo extends GroceryListInternal{
+  def main(args: Array[String]){
+    GROCERY_LIST
+    Watermelon
+    Watermelon
+    Lettuce
+    Chicken
+    Pasta
+    Noodles - 1
+    Fruit
+    Sriracha
+    Chicken
+    Eggs
+    Turkey
+    AND_PROBABLY_OTHER_STUFF
+  }
+}
