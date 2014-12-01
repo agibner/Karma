9e7425232ba25fdfbfda919c813fb1e3ffeb1670
Alex Gibner <agibner@utexas.edu>
2014-12-01 16:33:52 -0600

    starting test suite?

---
+++ src/GroceryTest.scala
@@ -0,0 +1,52 @@
+package groceryinternal
+
+object Test_Jump extends GroceryListInternal {
+  def main(args: Array[String]): Unit = {
+  	GROCERY_LIST
+
+  	Turkey
+  	AND_PROBABLY_OTHER_STUFF
+  }
+}
+
+object Test_Loop extends GroceryListInternal {
+  def main(args: Array[String]): Unit = {
+  	GROCERY_LIST
+
+  	Turkey
+  	AND_PROBABLY_OTHER_STUFF
+  }
+}
+
+object Test_Print extends GroceryListInternal {
+	def main(args: Array[String]): Unit = {
+  	GROCERY_LIST
+ 		Noodles - 72
+  	Pasta
+  	Noodles - 101
+  	Pasta
+  	Noodles - 108
+  	Pasta
+  	Noodles - 108
+  	Pasta
+  	Noodles - 11
+  	Pasta
+	  Noodles - 32
+	  Pasta
+	  Noodles - 87
+	  Pasta
+	  Noodles - 111
+	  Pasta
+	  Noodles - 114
+	  Pasta
+	  Noodles - 108
+	  Pasta
+	  Noodles - 100
+	  Pasta
+	  Noodles - 46
+	  Pasta
+		Turkey
+		AND_PROBABLY_OTHER_STUFF
+  }
+}
+
