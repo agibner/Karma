3f1e9e04dbe879df875d9510a71a608bbe5bffba
chance raine <chanceraine@gmail.com>
2014-12-02 17:21:59 -0600

    cleaned up internal (now its stupid) and added test suite start

--- src/GroceryListInternal.scala
+++ src/GroceryListInternal.scala
@@ -5,152 +5,110 @@ import scala.util.Random
 import scala.math.{ min, max }
 
 class GroceryListInternal {
-  abstract sealed class GroceryLine
-  case class End(num: Int) extends GroceryLine
-
-  var current: Int = 1
-
-  var lines = new HashMap[Int, GroceryLine]
-  var stack = new Stack[Int];
-  var karmaString = "";
-
-  // evaluate
-  private def gotoLine(line: Int) {
-
-    lines(line) match {
-      case End(_) =>
-      case _ =>
-    }
-  }
-
-  // end of the program
-  def AND_PROBABLY_OTHER_STUFF() = {
-    lines(current) = End(current)
-    //lines should be populated with Karma lines or something
-    //this should then call karma executor
-    gotoLine(lines.keys.toList.sorted.head);
-    val karmaEx = new KarmaExecuter();
-    System.out.println("\nKarma code:");
-    System.out.println(karmaString);
-    System.out.println("\nProgram:");
-    karmaEx.executeProgram(karmaString.split("\n"));
-  }
-  // beginning of program
-  def GROCERY_LIST() = {
-    lines = new HashMap[Int, GroceryLine];
-    stack = new Stack[Int];
-    karmaString = "0[1,";
-  }
+	var karmaString = "";
   
-  def Apples() = {
-    karmaString += GroceryListCompiler.convert('A',1);
-    current += 1
-  }
+	// end of the program
+	def AND_PROBABLY_OTHER_STUFF() = {
+		val karmaEx = new KarmaExecuter();
+		System.out.println("\nKarma code:");
+		System.out.println(karmaString);
+		System.out.println("\nProgram:");
+		karmaEx.executeProgram(karmaString.split("\n"));
+	}
+	
+	// beginning of program
+	def GROCERY_LIST() = {
+	    karmaString = "0[1,";
+	}
+	  
+	def Apples() = {
+	    karmaString += GroceryListCompiler.convert('A',1);
+	}
 
 	def Bananas() = {
 	    karmaString += GroceryListCompiler.convert('B',1);
-	    current += 1
-	 }
+	}
 	
 	def Chicken() = {
 	    karmaString += GroceryListCompiler.convert('C',1);
-	    current += 1
-	  }
+	}
 	
 	def Dumplings() = {
 	    karmaString += GroceryListCompiler.convert('D',1);
-	    current += 1
-	  }
+	}
 	
 	def Eggs() = {
 	    karmaString += GroceryListCompiler.convert('E',1);
-	    current += 1
-	  }
+	}
 	
 	def Fruit() = {
 	    karmaString += GroceryListCompiler.convert('F',1);
-	    current += 1
-	  }
+	}
 	
 	def Gruyere() = {
 	    karmaString += GroceryListCompiler.convert('G',1);
-	    current += 1
-	  }
+	}
 	
 	def Ibuprofen() = {
 	    karmaString += GroceryListCompiler.convert('I',1);
-	    current += 1
-	  }
+	}
 	
 	def Juice() = {
 	    karmaString += GroceryListCompiler.convert('J',1);
-	    current += 1
-	  }
+	}
 	
 	def Lettuce() = {
 	    karmaString += GroceryListCompiler.convert('L',1);
-	    current += 1
-	  }
+	}
 	
 	def Milk() = {
 	  karmaString += GroceryListCompiler.convert('M',1);
-	  current += 1
-	  }
+	}
 	
 	object Noodles {
 		def -(n: Int) = {
 		  karmaString += GroceryListCompiler.convert('N',n);
-		  current += 1
 		}
   	}	
 	
 	def Oatmeal() = {
 	  karmaString += GroceryListCompiler.convert('O',1);
-	    current += 1
-	  }
+	}
 	
 	def Pasta() = {
 	  karmaString += GroceryListCompiler.convert('P',1);
-	    current += 1
-	  }
+	}
 	
 	def Rice() = {
 	  karmaString += GroceryListCompiler.convert('R',1);
-	    current += 1
-	  }
+	}
 	
 	def Sriracha() = {
 	  karmaString += GroceryListCompiler.convert('S',1);
-	    current += 1
-	  }
+	}
 	
 	def Turkey() = {
 	  karmaString += GroceryListCompiler.convert('T',1);
-	    current += 1
-	  }
+	}
 	
 	def Utensils() = {
 	  karmaString += GroceryListCompiler.convert('U',1);
-	    current += 1
-	  }
+	}
 	
 	def Veggies() = {
 	  karmaString += GroceryListCompiler.convert('V',1);
-	    current += 1
-	  }
+	}
 	
 	def Watermelon() = {
 	    karmaString += GroceryListCompiler.convert('W',1);
-	    current += 1
-	  }
+	}
 	
 	def Xanax() = {
 	  karmaString += GroceryListCompiler.convert('X',1);
-	    current += 1
-	  }
+	}
 	
 	def Zucchini() = {
 	  karmaString += GroceryListCompiler.convert('Z',1);
-	    current += 1
-	  }
+	}
+	
 }
--- src/KarmaTester.scala
+++ src/KarmaTester.scala
@@ -1,3 +1,38 @@
-class KarmaTester {
+package groceryinternal
 
+import java.util._
+import java.io._
+import java.awt.Robot;
+import java.awt.event.KeyEvent
+
+object KarmaTester {
+	def main(args:Array[String]){		
+		runStackTest("1","?",'1');
+		runStackTest("11","??+",'1' + '1');
+		runStackTest("\1\1","??+",2);
+		runStackTest("11","??-",0);
+		runStackTest("\2\2","??*",4);
+		runStackTest("11","??/",1);
+		runStackTest("11","??%",0);
+		runStackTest("\1\1","??&",1);
+		runStackTest("\1\1","??|",1);
+		runStackTest("\1\1","??^",0);
+		runStackTest("\0","?!",1);
+		runStackTest("\1","?!",0);
+	}
+	
+	def runStackTest(data:String,program:String,endStack:Char) {
+		val karmEx = new KarmaExecuter();
+		var stdin = System.in;
+		try {
+			System.setIn(new ByteArrayInputStream(data.getBytes()));
+			karmEx.executeProgram(Array(program));
+			assert(karmEx.stack.pop() == endStack);
+			System.out.println(program + " with input " + data + " passed.");
+		} finally {
+			System.setIn(stdin);
+		}
+	}
 }
+
+
