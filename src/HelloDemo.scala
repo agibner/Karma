1b22c1be741ed4f055ea5abd8dbccea23dc89d97
chance raine <chanceraine@gmail.com>
2014-12-02 21:58:10 -0600

    added magic pt 2

--- src/HelloDemo.scala
+++ src/HelloDemo.scala
@@ -3,6 +3,9 @@ package groceryinternal
 object HelloDemo extends GroceryListInternal{
   def main(args: Array[String]){
     GROCERY_LIST
+    
+    //You can build programs with the built in names easily
+    //Doesn't require thinking about foods and can be helpful for remembering what things do
     Noodles - 72
     Pasta
     Noodles - 101
@@ -23,10 +26,15 @@ object HelloDemo extends GroceryListInternal{
     Pasta
     Noodles - 108
     Pasta
-    Noodles - 100
-    Pasta
-    Noodles - 46
-    Pasta
+    
+    //you can use the build in names this way too
+    some Noodles 100
+    a Pasta
+    
+    //example of how to use dynamic names
+    some Nutella 46
+    a Potato
+    
     Noodles - 10
     Pasta
     a Turtle
